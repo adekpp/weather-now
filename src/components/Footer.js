e5ac233f14a227a34488e1997aae928cc9646dda
adekpp <adekpp@gmail.com>
2022-01-20 12:40:43 +0100

    commited font issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
       <div className='absolute bottom-10 flex flex-row  w-full place-content-center gap-4'>
 
-  <div className='text-5xl text-white drop-shadow-lg hover:text-sky-500 md:text-xl'><a href='https://github.com/adekpp' target="blank"><FontAwesomeIcon className='mr-2' icon={['fab', 'github']} /></a></div>
-  <div className='text-5xl text-white drop-shadow-lg hover:text-sky-500 md:text-xl'><a href='https://www.linkedin.com/in/adrian-pietryga' target="blank"><FontAwesomeIcon className='mr-2' icon={['fab', 'linkedin']} /></a></div>
+  <div className='text-xl text-white drop-shadow-lg hover:text-sky-500 md:text-5xl'><a href='https://github.com/adekpp' target="blank"><FontAwesomeIcon className='mr-2' icon={['fab', 'github']} /></a></div>
+  <div className='text-xl text-white drop-shadow-lg hover:text-sky-500 md:text-5xl'><a href='https://www.linkedin.com/in/adrian-pietryga' target="blank"><FontAwesomeIcon className='mr-2' icon={['fab', 'linkedin']} /></a></div>
   </div>
 
 )};
